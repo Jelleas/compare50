fd12dfe326e40b21875b65deae4df125d60f6d42
jelleas <jelle.van.assema@gmail.com>
2022-08-02 16:40:04 +0200

    rm _home_renderer & _matcH_renderer

--- compare50/_renderer/_renderer.py
+++ compare50/_renderer/_renderer.py
@@ -1,6 +1,5 @@
 import collections
 import pathlib
-import json
 from re import sub
 import pkg_resources
 
@@ -10,11 +9,12 @@ TEMPLATES = pathlib.Path(pkg_resources.resource_filename("compare50._renderer", 
 from typing import Dict, List, Union, Any
 
 from ._cluster import Cluster
-from .. import _api, Compare50Result, Pass
-from ._home_renderer import get_home_data
-from ._match_renderer import get_match_data
+from .. import _api, Compare50Result, Pass, Explanation, IdStore, Span, FileSubmission, File, Group
 
 import jinja2
+
+
+__all__ = ["render"]
 
 
 def render(
@@ -40,10 +40,10 @@ def render(
     # Create the directory if it does not yet exist
     dest.mkdir(exist_ok=True)
 
-    return _render_multi(sub_pair_to_results, cluster, dest)
+    return render_multi(sub_pair_to_results, cluster, dest)
 
 
-def _render_multi(
+def render_multi(
     sub_pair_to_results: Dict[Pass, List[Compare50Result]],
     cluster: Cluster,
     dest: pathlib.Path
@@ -83,7 +83,7 @@ def _render_multi(
     return home_path
 
 
-def _render_bundled(
+def render_bundled(
     sub_pair_to_results: Dict[Pass, List[Compare50Result]],
     cluster: Cluster,
     dest: pathlib.Path
@@ -108,7 +108,7 @@ def _render_bundled(
         progress_bar.update()
 
     # Render home page
-    home_data = render_home_data(cluster)
+    home_data = get_home_data(cluster)
 
     data = {
         "matches": matches_data 
@@ -132,3 +132,109 @@ def _render_page(data: Any, filename: str) -> str:
     with open(STATIC / filename) as f:
         page = f.read()
     return f"{rendered_data}\n{page}"
+
+
+def get_home_data(cluster: Cluster) -> str:
+    return {
+        "SUBMISSIONS": cluster.submissions_as_dict(),
+        "LINKS": cluster.links_as_dict()
+    }
+
+
+def get_match_data(
+    sub_a: FileSubmission,
+    sub_b: FileSubmission,
+    results: List[Compare50Result],
+    cluster: Cluster,
+    metadata: Dict
+) -> str:
+    files_a = files_as_dict(sub_a)
+    files_b = files_as_dict(sub_b)
+
+    span_id_store: IdStore[Span] = IdStore()
+    group_id_store: IdStore[Group] = IdStore()
+
+    passes = [pass_as_dict(result, span_id_store, group_id_store) for result in results]
+
+    match_data = {
+        "METADATA": metadata,
+        "SUB_A": files_a,
+        "SUB_B": files_b,
+        "PASSES": passes,
+        "SUBMISSIONS": cluster.submissions_as_dict(),
+        "LINKS": cluster.links_as_dict()
+    }
+
+    return match_data
+
+
+def file_as_dict(file: File) -> Dict:
+    return {
+        "id": file.id,
+        "name": str(file.name),
+        "language": file.lexer().name,
+        "content": file.read()
+    }
+
+
+def files_as_dict(submission: FileSubmission) -> Dict:
+    return {
+        "id": submission.id,
+        "name": str(submission.path),
+        "isArchive": submission.is_archive,
+        "files": [file_as_dict(file) for file in submission.files]
+    }
+
+
+def span_as_dict(span: Span, span_id_store: IdStore, ignored: bool=False) -> Dict:
+    return {
+        "id": span_id_store[span],
+        "subId": span.file.submission.id,
+        "fileId": span.file.id,
+        "start": span.start,
+        "end": span.end,
+        "ignored": ignored
+    }
+
+
+def pass_as_dict(result: Compare50Result, span_id_store: IdStore, group_id_store: IdStore) -> Dict:
+    spans = []
+    groups = []
+
+    for group in result.groups:
+        id = group_id_store[group]
+        grouped_span_ids: List[int] = []
+
+        for span in group.spans:
+            grouped_span_ids.append(span_id_store[span])
+            spans.append(span_as_dict(span, span_id_store))
+
+        groups.append({"id": id, "spans": grouped_span_ids})
+
+    for span in result.ignored_spans:
+        spans.append(span_as_dict(span, span_id_store, ignored=True))
+
+    explainers: List[Dict] = [
+        {
+            "name" : explainer.name,
+            "explanations": [explanation_as_dict(exp, span_id_store) for exp in explanations]
+        }
+        for explainer, explanations in result.explanations.items()
+    ]
+
+    return {
+        "name": result.pass_.__name__, # type: ignore
+        "docs": result.pass_.__doc__,
+        "score": result.score.score,
+        "spans": spans,
+        "groups": groups,
+        "explainers": explainers
+    }
+
+
+def explanation_as_dict(explanation: Explanation, span_id_store: IdStore[Span]) -> Dict:
+    return {
+        "span": span_as_dict(explanation.span, span_id_store),
+        "text": explanation.text,
+        "weight": explanation.weight,
+    }
